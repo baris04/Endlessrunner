faef86dd982bda6ddef7c0f844924bbe7a61918c
baris04 <barisbasaran2004@hotmail.com>
2024-04-06 00:13:42 +0300

    updated
    new movement

--- Assets/Charactermovement.cs
+++ Assets/Charactermovement.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class Charactermovement : MonoBehaviour
 {
-    [SerializeField] public float movespeed;
-    [SerializeField] public float jumpamount;
+
+
+    public enum pos {
+        left, //-2
+        mid, // 1 
+        right // 4 
+
+    }
+    public float movespeed;
+    public float jumpamount;
+    bool isGrounded;
+    public pos position;
+    float lerpedx=1f;      
+    public pos[] Currentpos = {pos.left, pos.mid , pos.right };
+    int pointer;
+
 
     
 
+    
+
+
+    
 
     void Start()
     {
@@ -17,18 +36,76 @@ public class Charactermovement : MonoBehaviour
     }
 
     void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.A))
+{
+    pointer = Mathf.Clamp(pointer - 1, 0, 2);
+    position = Currentpos[pointer];
+}
+     if (Input.GetKeyDown(KeyCode.D))
+{
+    pointer = Mathf.Clamp(pointer + 1, 0, 2);
+    position = Currentpos[pointer];
+}
+
+
+
+
+
+
+
+
+        switch(position)
     {
-        float moveamounthorizontal = Input.GetAxis("Horizontal")* Time.deltaTime * movespeed ;
+        case pos.left:
+            lerpedx = Mathf.Lerp(transform.position.x , -2, 0.1f);
+            transform.position = new Vector3(lerpedx, transform.position.y,transform.position.z);
+            break;
+
+        case pos.mid:
+            lerpedx = Mathf.Lerp(transform.position.x , 1, 0.1f);
+            transform.position = new Vector3(lerpedx,transform.position.y,transform.position.z);
+            break;
+
+        case pos.right:
+            lerpedx = Mathf.Lerp(transform.position.x , 4, 0.1f);
+            transform.position = new Vector3(lerpedx ,transform.position.y,transform.position.z);
+            break;
+    }
+
+        
+       // float moveamounthorizontal = Input.GetAxis("Horizontal")* Time.deltaTime * movespeed ;
         float moveamountvertical =  Time.deltaTime * movespeed ;
 
         transform.Translate(0,0,moveamountvertical);  
-        transform.Translate(moveamounthorizontal,0,0);
+       // transform.Translate(moveamounthorizontal,0,0);
        
-        if (Input.GetAxis("Jump")!=0){
+        if (Input.GetAxis("Jump")!=0 && isGrounded ){
         GetComponent<Rigidbody>().velocity = new Vector3(0, jumpamount, 0);
-}
-       
-        
-
+        GetComponent<Animator>().SetTrigger("Jump");
+        }
+    
+    if(Input.GetKeyDown(KeyCode.S))
+    {
+        GetComponent<Animator>().SetTrigger("Crouch");
     }
-}
+       
+    }
+
+   
+    
+    
+
+    void OnCollisionEnter(Collision Other)
+    {
+        isGrounded= true;
+        GetComponent<Animator>().SetBool("isInAir",false) ;
+    }
+    
+    void OnCollisionExit(Collision Other)
+    
+    {
+        isGrounded= false ;
+        GetComponent<Animator>().SetBool("isInAir",true) ;
+    }
+    }
